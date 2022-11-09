e2a73d1ee44d2fff0628a51fb0b8b8f210710548
hellnomary <maria.porcires@outlook.com>
2022-11-09 12:29:37 +0100

    update

---
+++ project/script.js
@@ -0,0 +1,20 @@
+const BASE_URL = 'https://fathomless-shelf-54969.herokuapp.com';
+
+async function getKey() {
+    const response = await fetch(`${BASE_URL}/keys`, { method: 'POST' });
+    const data = await response.json();
+    console.log(data);
+}
+
+async function getPlanets() {
+    const response = await fetch(`${BASE_URL}/bodies`, {
+        headers: {
+            'x-zocom': 'solaris-vKkkQHqQboi7c6JF'
+        }
+    });
+    const data = await response.json();
+    console.log(data);
+}
+
+getKey();
+getPlanets();
