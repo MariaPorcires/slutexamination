49e750280cf10793b0e4a8e44c07993347e87b6b
hellnomary <maria.porcires@outlook.com>
2022-11-11 09:11:51 +0100

    update

--- project/script.js
+++ project/script.js
@@ -1,9 +1,9 @@
 const BASE_URL = 'https://fathomless-shelf-54969.herokuapp.com';
+let planetData;
 
 async function getKey() {
     const response = await fetch(`${BASE_URL}/keys`, { method: 'POST' });
     const data = await response.json();
-    console.log(data);
 }
 
 async function getPlanets() {
@@ -13,8 +13,34 @@ async function getPlanets() {
         }
     });
     const data = await response.json();
+    return data;
     console.log(data);
 }
 
-getKey();
-getPlanets();
+
+async function start(){
+    getKey();
+    planetData = await getPlanets(); //sparar info om planeter i variabel
+    createEvents(); //skapa events för planeterna
+}
+
+function createEvents() { //själva funktionen för events
+    const elementMain = document.querySelector(`main`); // hämta main
+    const buttons = elementMain.querySelectorAll(`button`); // hämta alla knappar
+    buttons.forEach(function(button){ //funktion för varje klick, ska loopa igenom alla
+        button.addEventListener(`click`, buttonClick);
+    });
+}
+
+function buttonClick(event) { //för att hämta rätt info till rätt planet
+    const button = event.currentTarget;
+    const planetId = button.id;
+console.log(planetId);
+console.log(planetData);
+}
+
+start();
+
+
+
+
